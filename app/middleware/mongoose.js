d83f32074931749b74b4a22f6d6a2dad55e86b63
byau <billy.l.yau@gmail.com>
2016-03-11 15:05:01 -0500

    first commit

---
+++ app/middleware/mongoose.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/nozama-api';
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
