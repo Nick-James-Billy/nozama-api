aece7f4f9275415640d9dfcbe5a2697a3d7a5c1f
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-03-11 17:29:23 -0500

    adds initial purchase model

---
+++ app/models/purchase.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const purchaseSchema = new mongoose.Schema({
+  items: [],
+  _userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false
+},
+}, {
+  timestamps: true,
+});
+
+
+const Purchase = mongoose.model('Purchase', purchaseSchema);
+
+module.exports = Purchase;
