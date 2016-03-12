3385d4b53d7984824eb10eb7de04ce4898efa092
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-03-12 14:29:17 -0500

    adds initial purchase controller,
    will need revisions after testing

--- app/controllers/purchases.js
+++ app/controllers/purchases.js
@@ -8,6 +8,7 @@ const authenticate = require('./concerns/authenticate');
 
 const index = (req, res, next) => {
   Purchase.find()
+  // Purchase.find({_userId: req.currentUser._id})
     .then(purchases => res.json({ purchases }))
     .catch(err => next(err));
 };
@@ -17,3 +18,50 @@ const show = (req, res, next) => {
     .then(purchase => purchase ? res.json({ purchase }) : next())
     .catch(err => next(err));
 };
+
+const create = (req, res, next) => {
+  let purchase = Object.assign(req.body.purchase, {
+    _userId: req.currentUser._id,
+  });
+  Purchase.create(purchase)
+    .then(purchase => res.json({ purchase }))
+    .catch(err => next(err));
+};
+
+const update = (req, res, next) => {
+  let search = { _id: req.params.id, _userId: req.currentUser._id };
+  Purchase.findOne(search)
+    .then(purchase => {
+      if (!purchase) {
+        return next();
+      }
+
+      return purchase.update(req.body.purchase)
+        .then(() => res.sendStatus(200));
+    })
+    .catch(err => next(err));
+};
+
+const destroy = (req, res, next) => {
+  let search = { _id: req.params.id, _userId: req.currentUser._id };
+  Purchase.findOne(search)
+    .then(purchase => {
+      if (!purchase) {
+        return next();
+      }
+
+      return purchase.remove()
+        .then(() => res.sendStatus(200));
+    })
+    .catch(err => next(err));
+};
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy,
+}, { before: [
+  { method: authenticate },
+], });
