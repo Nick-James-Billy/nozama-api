a1cf2c1226a0c2405b27b543ac2d68944a5f1e5e
jpfriedman18 <jpfriedman18@gmail.com>
2016-03-12 18:02:29 -0500

    Fix Multer formdata issue on Users controller Note: NPM INSTALL AFTER PULLING!

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:3000/sign-out/$ID \
-  --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE http://localhost:3000/sign-out/56e4687b1a0457660e7bec13 \
+  --header "Authorization: Token token=MQjpLIHO3+HNBO12jvR+Dg=="
